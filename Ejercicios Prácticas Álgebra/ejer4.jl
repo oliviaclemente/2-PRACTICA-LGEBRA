5255ff61d86f3de1ee7e4f043722f6cde1840129
yeahyeah <113980774+mariaalonsan@users.noreply.github.com>
2023-12-09 22:39:52 +0100

    jhbajkHJZSl

--- Ejercicios Prácticas Álgebra/ejer4.jl
+++ Ejercicios Prácticas Álgebra/ejer4.jl
@@ -1,6 +1,6 @@
 """
 Crear una funcion markov(A, v, k) que reciba como 
-par ́ametros una matriz A talque las coordenadas de 
+parametros una matriz A talque las coordenadas de 
 cada columna suman 0, un vector de probabilidad v 
 (suscoordenadas suman 0) y un n ́umero natural k y 
 devuelva el valor del paso k de lacadena de Markov 

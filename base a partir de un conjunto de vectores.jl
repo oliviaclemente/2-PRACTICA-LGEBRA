d18c013bbb604fb2565a76ebb8fc9a99d4ff4697
yeahyeah <113980774+mariaalonsan@users.noreply.github.com>
2023-12-09 12:44:49 +0100

    SUIDCSJMJC

--- base a partir de un conjunto de vectores.jl
+++ base a partir de un conjunto de vectores.jl
@@ -1,16 +1,45 @@
 using LinearAlgebra
+
+function are_li(V)
+    A = hcat(V...)  # Convertir los vectores en una matriz
+    A = float(A)  # Convertir la matriz a tipo flotante
+    m, n = size(A)
+    for j = 1:n
+        pivot = findfirst(!iszero, A[j:m, j])
+        if pivot === nothing
+            return false
+        end
+        pivot += j - 1
+
+        if pivot != j
+            A[j, :], A[pivot, :] = A[pivot, :], A[j, :]
+        end
+
+        for i = j+1:m
+            A[i, :] -= A[i, j] / A[j, j] * A[j, :]
+        end
+    end
+
+    return true
+end
+
 function find_basis(S)
-    A=[S[1]]
-    n=length(S)
-    D=[S[1]]
-    for i in 2:n
-        B=[S[i]]
-        C=vcat(A,B)
-        if are_li(i)!=0
-            D=vcat(A,B)
+    A = [S[1]]
+    D = [S[1]]
+
+    for i in 2:length(S)
+        B = [S[i]]
+        C = vcat(A, B)
+        if are_li(C)
+            push!(D, B[1])
+            A = C
         end
     end
     return D
 end
 
-
+# Ejemplo
+S = [[6, 0, 2], [1, 4, 9], [8, 7, 4]]
+base = find_basis(S)
+println("La base encontrada es: ")
+println(base)
